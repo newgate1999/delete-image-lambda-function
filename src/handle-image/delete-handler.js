4943f3a5f97598b8a445afb168f2dab5ec3364d4
newgate <tuananh301099@gmail.com>
2021-03-16 10:39:42 +0700

    handler event delete image

--- src/handle-image/delete-handler.js
+++ src/handle-image/delete-handler.js
@@ -4,43 +4,31 @@ const sharp = require('sharp');
 const path = require('path');
 require('dotenv').config();
 
-exports.resizeImage = async (event, context) => {
-  // console.log(
-  //   'Reading options from event:\n',
-  //   util.inspect(event, {
-  //     depth: 5,
-  //   }),
-  // );
+exports.deleteImage = async (event, context) => {
   for (const record of event.Records) {
     console.log('1');
     const sourceKey = decodeURIComponent(
-      record.s3.object.key.replace(/\+/g, ' '),
+        record.s3.object.key.replace(/\+/g, ' '),
     );
     console.log('2');
     if (validFile(sourceKey)) {
       const nameFile = path.basename(sourceKey);
+      const sourceKeyArr = sourceKey.split('/');
+      const profile = sourceKeyArr[1];
       const allSizes = process.env.ALL_SIZE.split(',');
       console.log(sourceKey);
       try {
-        const originalImage = await getImage(sourceKey);
         console.log('4 ' + 'Total size: ' + allSizes.length);
-
         const sizes = allSizes.map((size) => takeSize(size));
-        const imagesProcessed = await Promise.all(
-          sizes.map((size) => {
-            return handleImage(originalImage.Body, size);
-          }),
-        );
         console.log("resized");
         const dataMapping = sizes.map((size, index) => {
           return {
-            targetKey: `resized/${size.width}x${size.height}/` + nameFile,
-            buffer: imagesProcessed[index],
+            targetKey: `RESIZED/PROFILE/` + profile + `/${size.width}x${size.height}/`+ nameFile,
           };
         });
         console.log("mapping");
-        const imagesPut = await Promise.all(dataMapping.map(data => {
-          return putImage(data.buffer, data.targetKey)
+        const imagesDelete = await Promise.all(dataMapping.map(data => {
+          return deleteImage(data.targetKey)
         }));
         console.log('successful');
       } catch (error) {
@@ -69,26 +57,11 @@ function validFile(sourceKey) {
   }
   return true;
 }
-function getImage(sourceKey) {
-  const params = {
-    Bucket: process.env.BUCKET,
-    Key: sourceKey,
-  };
-  console.log('3');
-  return s3.getObject(params).promise();
-}
-function handleImage(body, size) {
-  // return new Promise((resolve) =>{
-  //   return resolve(sharp(body).resize(size.width, size.height).toBuffer())
-  // });
-  return sharp(body).resize(size.width, size.height).toBuffer();
-}
-function putImage(body, targetKey) {
+
+function deleteImage(targetKey) {
   const targetParams = {
     Bucket: process.env.BUCKET,
     Key: targetKey,
-    Body: body,
-    ContentType: 'image',
   };
-  return s3.putObject(targetParams).promise();
+  return s3.deleteObject(targetParams).promise();
 }
